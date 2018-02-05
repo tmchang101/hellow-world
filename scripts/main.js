98b9f0fb93d8d65503b3c30d34e5e864a850b738
tmchang101 <36144833+tmchang101@users.noreply.github.com>
2018-02-05 14:55:38 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
